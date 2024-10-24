986802509235247a06bdd52065c71c5ac0f3d7f7
Vladislav Panov <137095641+vp-174@users.noreply.github.com>
2024-10-24 08:32:21 +0500

    Update

--- src/vpGoogleTranslate.h
+++ src/vpGoogleTranslate.h
@@ -2,26 +2,7 @@
  * Библиотека: vpGoogleTranslate
  * Автор: Vladislav Panov
  * Версия: 1.0
- * 
- * Описание:
- * vpGoogleTranslate - это C++ библиотека, предназначенная для выполнения переводов текста с использованием 
- * API Google Translate. Библиотека использует cURL для выполнения HTTP-запросов и nlohmann/json (Niels Lohmann)
- * для обработки JSON-ответов.
- * 
- * Основные функции:
- * - translate: переводит заданный текст с одного языка на другой.
- * 
- * Использование:
- * 1. Создайте объект класса vpGoogleTranslate.
- * 2. Вызовите метод translate, передав текст для перевода, исходный язык и язык назначения.
- * 
- * Пример:
- * vp::vpGoogleTranslate translator;
- * std::string translatedText = translator.translate("Hello", "en", "ru");
- * std::cout << translatedText << std::endl; // Вывод: "Привет"
- * 
- * Лицензия: MIT License
- */
+*/
 
 #ifndef VPGOOGLETRANSLATE_H
 #define VPGOOGLETRANSLATE_H
